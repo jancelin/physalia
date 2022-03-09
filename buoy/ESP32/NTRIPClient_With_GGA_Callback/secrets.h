0b396231e237a517afcb8945088e7d0a29e2ec08
jancelin <julien.ancelin@inra.fr>
2022-03-09 17:46:14 +0100

    esp32 add generic secrets.h

---
+++ buoy/ESP32/NTRIPClient_With_GGA_Callback/secrets.h
@@ -0,0 +1,20 @@
+//Your WiFi credentials
+const char ssid[] = "BUOY";
+const char password[] = "12345678";
+
+//MQTT connexion
+const char* mqttServer = "127.0.0.1";
+const int mqttPort = 1883;
+const char* mqttUser = "";
+const char* mqttPassword = "";
+const char* mqtttopic = "buoy/gnss";
+
+//material uuid
+const char matUuid[] = "'Buoy_eps32_f9p_rtk'";
+
+//Centipede works well and is free
+const char casterHost[] = "caster.centipede.fr";
+const uint16_t casterPort = 2101;
+const char casterUser[] = "centipede32"; //User must provide their own email address to use RTK2Go
+const char casterUserPW[] = "centipede32";
+const char mountPoint[] = "LIENSS"; //The mount point you want to get data from
