fc3636a8159fca576eca25ed3388e90ea055c392
DeGaMon <quentin.gchnk@gmail.com>
2023-09-13 15:46:39 +0200

    Set default WiFiinfo and MQTT IP

--- buoy_mavi/esp32/secrets.h
+++ buoy_mavi/esp32/secrets.h
@@ -1,16 +1,16 @@
 //Your WiFi credentials
-const char ssid[] = "wifi_hotspot";
-const char password[] = "password";
+const char ssid[] = "buoy";
+const char password[] = "Buoy_43210!";
 
 //MQTT connexion
-const char* mqttServer = "xxx.xxx.xxx.xxx";
+const char* mqttServer = "127.0.0.1";
 const int mqttPort = 8090;
 const char* mqttUser = "";
 const char* mqttPassword = "";
-const char* mqtttopic = "buoy/gnss";
+const char* mqtttopic = "buoy/gnss_test";
 
 //material uuid
-const char matUuid[] = "'ESP32_";
+const char matUuid[] = "'someName_";
 
 //Centipede works well and is free
 const char casterHost[] = "caster.centipede.fr";
