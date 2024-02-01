5048d38fd8990e415fc99bf6d96d1d6e5384af64
Romain <rtourte@yahoo.fr>
2024-02-01 15:50:48 +0100

    Update RTK ACQUISITION

--- buoy_mavi/esp32/buoy_mavi_lte/secrets.h
+++ buoy_mavi/esp32/buoy_mavi_lte/secrets.h
@@ -11,7 +11,7 @@
 #define uS_TO_S_FACTOR 1000000
 
 int TIME_TO_SLEEP = 30; // temps de repos en deepsleep.
-int RTK_ACQUISITION_PERIOD = 30; // Temps pendant lequel on doit capter de la donnée en RTK ( secondes )
+int RTK_ACQUISITION_PERIOD = 120; // Temps pendant lequel on doit capter de la donnée en RTK ( secondes )
 int RTK_MAX_RESEARCH = 120; // Temps max pendant lequel le dispositif recherche du RTK ( seconds )
 
 RTC_DATA_ATTR int lastPeriodRecord = 0;
