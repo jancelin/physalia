5a02a5c8f40649ad66c2f05faf6302f8d0d78a18
jancelin <julien.ancelin@inra.fr>
2024-02-13 16:26:45 +0100

    intégration du code pour une Lilygo T-pcie + SIM7600G

---
+++ buoy_mavi/esp32/buoy_sim7600_rtk/README.md
@@ -0,0 +1,20 @@
+## LILYGO T-Pcie + SIM7600G
+
+![Lilygo T-pcie + sim7600g](https://github.com/Xinyuan-LilyGO/LilyGo-T-PCIE/raw/master/image/pins.jpg)
+
+https://github.com/Xinyuan-LilyGO/LilyGo-T-PCIE/tree/master
+
+* Material:
+  - [ESP32 with Pcie + bat: Lilygo T-pcie](https://www.tinytronics.nl/shop/en/development-boards/microcontroller-boards/with-wi-fi/lilygo-t-pcie-v1.2-axp2101-esp32-wrover-16mb)
+  - [LTE 4G: SIM7600g](https://www.tinytronics.nl/shop/en/communication-and-signals/wireless/gps/modules/lilygo-ttgo-t-pcie-sim7600g-h-expansion-module)
+  - [F9P: drotek DP0601](https://store-drotek.com/891-rtk-zed-f9p-gnss.html)
+  - [relay: 5V 1-channel high-active](https://www.tinytronics.nl/shop/en/switches/relays/5v-relay-1-channel-high-active)
+  - [battery: Li-Po Battery 3.7V 2000mAh](https://www.tinytronics.nl/shop/en/power/batteries/li-po/li-po-accu-3.7v-2000mah)
+  
+
+### Arduino IDE
+
+![image](https://github.com/jancelin/physalia/assets/6421175/4a6827f4-2eff-4e9a-9c4c-cf8777e0086d)
+
+
+
---
+++ buoy_mavi/esp32/buoy_sim7600_rtk/secrets.h
@@ -0,0 +1,54 @@
+/*
+=============================================================================================
+PHYSALIA
+  - Plateforme HYdrographique pour la Surveillance Altimétrique du LIttoral
+  - Hydrographic Platform for Altimetric Monitoring of the Coastal Zone
+
+* Souce: https://github.com/jancelin/physalia
+* License: GNU Affero General Public License v3.0
+
+* Parameter file for the buoy_sim7600_rtk.ino
+=============================================================================================
+*/
+//Material uuid
+const char matUuid[] = "'TESTv3'";
+
+//RTK connection
+const char mountPoint[] = "CT"; //The mount point you want to get data from
+const char casterHost[] = "caster.centipede.fr";
+const uint16_t casterPort = 2101 ;
+const char casterUser[] = "mavi";
+const char casterUserPW[] = "mavi";
+const bool transmitLocation = false; //Send gga to caster
+
+//MQTT connexion
+const char* mqttServer = "mavi-mqtt.centipede.fr";
+const int mqttPort = 8090;
+const char* mqttUser = "";
+const char* mqttPassword = "";
+const char* mqtttopic = "buoy/mavi";
+const char* mqttbat = "buoy/bat";
+
+// GNSS acquisition Frequency ( Hz )
+int GNSS_FREQ = 1;
+
+// DEEP SLEEP CONFIGURATION
+bool DEEP_SLEEP_ACTIVATED = true;     // True = DeepSleep sinon DeepSleep ( off ) captation en continue
+int TIME_TO_SLEEP = 30; // temps de repos en deepsleep.
+int RTK_ACQUISITION_PERIOD = 120; // Temps pendant lequel on doit capter de la donnée en RTK ( secondes )
+int RTK_MAX_RESEARCH = 120; // Temps max pendant lequel le dispositif recherche du RTK ( secondes )
+#define uS_TO_S_FACTOR 1000000
+RTC_DATA_ATTR int lastPeriodRecord = 0;
+int ACQUISION_PERIOD_4G = 120; // Temps ( en seconde ) pendant lequel on va chercher le network 4G avant de faire un deepsleep( TIME_TO_SLEEP )
+
+// BAT
+int BAT_PERIOD = 10;    // Interval pour envoi de l'état de la batterie (en seconde )
+
+// HARDWARE CONNECTION
+#define pin_GNSS 33     // ANALOG PIN 33 ( Relais 1 )
+#define pin_GSM 32        // ANALOG PIN 33 ( Relais 2 )
+#define LDR 2  // composante photoresistor sur la pin GPI02
+
+//Your WiFi credentials
+//const char ssid[] = "blablabla";
+//const char password[] = "12345678";
